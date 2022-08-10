d1f0d17a6a515c890b766affa5b6706e6b43ff36
Nabcellent <nabcellent.dev@gmail.com>
2022-08-10 03:12:05 +0300

    Adds postcss plugin for rollup
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 
@@ -25,6 +26,7 @@ export default [
             typescript({ tsconfig: './tsconfig.json', }),
             peerDepsExternal(),
             terser(),
+            postcss()
         ]
     }, {
         input: 'dist/esm/index.d.ts',
