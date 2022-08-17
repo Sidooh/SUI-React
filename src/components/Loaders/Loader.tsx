1c587852c49bf503d457c56f3bdd1d21bc65f3d2
Nabcellent <nabcellent.dev@gmail.com>
2022-08-17 08:30:30 +0300

    Updates page loader
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/components/Loaders/Loader.tsx
+++ src/components/Loaders/Loader.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { Spinner } from 'react-bootstrap';
 
 const PageLoaderWrapper = styled('div')({
+    width: '100vw',
+    height: '100vh',
     position: 'fixed',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-
+    top: 0,
+    left: 0,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'var(--sidooh-body-bg)',
     zIndex: 1301,
+});
+
+const rotate = keyframes`
+  from {
+    transform: rotate(360deg);
+  }
+
+  to {
+    transform: rotate(0deg);
+  }
+`;
+
+const PageLoaderText = styled('h4')({
+    fontFamily: '"Pacifico", cursive',
+    animation: `${rotate} 7s linear infinite`,
+    position: 'absolute'
 });
 
 const SectionLoaderWrapper = styled.div({
@@ -37,11 +57,14 @@ const ComponentLoader = () => {
 };
 
 // ==============================|| LOADER ||============================== //
-const PageLoader = () => (
-    <PageLoaderWrapper>
-        <Spinner animation="border" variant="primary" style={{width: '7rem', height: '7rem'}}/>
-    </PageLoaderWrapper>
-);
+const PageLoader = () => {
+    return (
+        <PageLoaderWrapper>
+            <PageLoaderText>SIDOOH</PageLoaderText>
+            <Spinner animation="border" variant="primary" style={{width: '10rem', height: '10rem'}}/>
+        </PageLoaderWrapper>
+    );
+};
 
 const SectionLoader = () => {
     return (
