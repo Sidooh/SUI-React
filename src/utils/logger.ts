0516b41a9b2f67df46283a36a8cc95a424606b16
Nabcellent <nabcellent.dev@gmail.com>
2022-09-07 20:26:30 +0300

    Adds logger
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,8 +1,5 @@
 /** Log levels */
-export type LogLevel = 'log' | 'warn' | 'error';
-
-// @ts-ignore
-export const LOG_LEVEL: LogLevel = import.meta.env.VITE_LOG_LEVEL || (process.env.NODE_ENV === 'production' ? 'warn' : 'log');
+export type LogLevel = 'log' | 'warn' | 'error' | 'info' | 'debug';
 
 /** Signature of a logging function */
 export interface LogFn {
@@ -11,35 +8,57 @@ export interface LogFn {
 
 /** Basic logger interface */
 export interface Logger {
-    log: LogFn;
-    warn: LogFn;
     error: LogFn;
+    warn: LogFn;
+    info: LogFn;
+    debug: LogFn;
+    log: LogFn;
 }
 
 // @ts-ignore
-const NO_OP: LogFn = (message?: any, ...optionalParams: any[]) => {};
+const NO_OP: LogFn = (message?: any, ...optionalParams: any[]) => {
+};
 
 /** Logger which outputs to the browser console */
 export class ConsoleLogger implements Logger {
-    readonly log: LogFn;
-    readonly warn: LogFn;
     readonly error: LogFn;
+    readonly warn: LogFn;
+    readonly info: LogFn;
+    readonly debug: LogFn;
+    readonly log: LogFn;
 
     constructor(options?: { level?: LogLevel }) {
         const { level } = options || {};
 
         this.error = console.error.bind(console);
-
         if (level === 'error') {
             this.warn = NO_OP;
+            this.info = NO_OP;
+            this.debug = NO_OP;
             this.log = NO_OP;
 
             return;
         }
 
         this.warn = console.warn.bind(console);
-
         if (level === 'warn') {
+            this.info = NO_OP;
+            this.debug = NO_OP;
+            this.log = NO_OP;
+
+            return;
+        }
+
+        this.info = console.info.bind(console);
+        if (level === 'info') {
+            this.debug = NO_OP;
+            this.log = NO_OP;
+
+            return;
+        }
+
+        this.debug = console.debug.bind(console);
+        if (level === 'debug') {
             this.log = NO_OP;
 
             return;
@@ -48,5 +67,3 @@ export class ConsoleLogger implements Logger {
         this.log = console.log.bind(console);
     }
 }
-
-export const logger = new ConsoleLogger({ level: LOG_LEVEL });
