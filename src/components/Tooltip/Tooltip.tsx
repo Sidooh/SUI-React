7502fdb6c32c538a9b670e3c564ff68c87aa3503
Nabcellent <nabcellent.dev@gmail.com>
2022-09-09 06:15:28 +0300

    Updates tooltip
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useRef, useState } from 'react';
 import styled, { css } from 'styled-components';
 
 type Position = 'top' | 'left' | 'bottom' | 'right'
@@ -9,110 +9,133 @@ type TooltipProps = {
     placement?: Position
 }
 
-const TooltipTooltip = styled.span<{ placement: Position }>`
+const TooltipWrapper = styled.div`
   position: relative;
+  display: inline-flex;
+`;
 
-  &:after {
-    /*  The arrow */
-    content: "";
-    position: absolute;
-    display: none;
-    border: 7px solid transparent;
-  }
+const TooltipTarget = styled.span<{ showOnFocus: boolean }>`
+  border: none;
+  background: none;
+  padding: 5px;
+  margin: -1px;
+  color: inherit;
+  cursor: inherit;
+  display: flex;
+  font-size: inherit;
+  ${({ showOnFocus }) => !showOnFocus && css`outline: none`}
+`;
 
-  ${props => {
-    switch (props.placement) {
-      case 'top':
+const CenterContainer = styled.div<{ placement: Position }>`
+  position: absolute;
+  width: 200px;
+  margin-left: -100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  pointer-events: none;
+
+  ${({ placement }) => {
+    switch (placement) {
+      case 'bottom':
         return css`
-          & div {
-            bottom: 100%;
-            margin-bottom: 7px;
-          }
-
-          &:after {
-            left: 50%;
-            bottom: 100%;
-            margin-bottom: -7px;
-            transform: translateX(-50%);
-            border-top-color: rgba(15, 27, 76, .7);
-          }
-        `;
-      case 'right':
-        return css`
-          & div {
-            top: 50%;
-            left: 100%;
-            margin-left: 7px;
-            transform: translateY(-50%);
-          }
-
-          &:after {
-            top: 50%;
-            left: 100%;
-            margin-left: -7px;
-            transform: translateY(-50%);
-            border-right-color: rgba(15, 27, 76, .7);
-          }
+          top: calc(100% + 5px);
+          left: 50%;
         `;
       case 'left':
         return css`
-          & div {
-            top: 50%;
-            right: 100%;
-            margin-right: 7px;
-            transform: translateY(-50%);
-          }
-
-          &:after {
-            top: 50%;
-            right: 100%;
-            margin-right: -7px;
-            transform: translateY(-50%);
-            border-left-color: rgba(15, 27, 76, .7);
-          }
+          margin-left: 0;
+          top: 50%;
+          transform: translateY(-50%);
+          right: calc(100% + 5px);
+          width: max-content;
+        `;
+      case 'right':
+        return css`
+          margin-left: 0;
+          top: 50%;
+          transform: translateY(-50%);
+          left: calc(100% + 5px);
+          width: max-content;
         `;
       default:
         return css`
-          & div {
-            top: 100%;
-            margin-top: 7px;
-          }
-
-          &:after {
-            top: 100%;
-            left: 50%;
-            margin-top: -7px;
-            transform: translateX(-50%);
-            border-bottom-color: rgba(15, 27, 76, .7);
-          }
+          bottom: calc(100% + 5px);
+          left: 50%;
         `;
     }
   }}
-  &:hover div, &:hover:after {
-    display: block;
+`;
+
+const TooltipBox = styled.span<{ placement: Position }>`
+  position: relative;
+  background-color: rgba(var(--sidooh-dark-rgb), .7);
+  color: #fff;
+  text-align: center;
+  border-radius: .3rem;
+  padding: 10px 8px;
+  font-size: 14px;
+  box-shadow: var(--sidooh-box-shadow);
+
+  &:after {
+    content: '';
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: rgba(var(--sidooh-dark-rgb), .7) transparent transparent transparent;
+
+    ${({ placement }) => {
+      switch (placement) {
+        case 'bottom':
+          return css`
+            border-color: transparent transparent rgba(var(--sidooh-dark-rgb), .7) transparent;
+            bottom: 100%;
+            left: calc(50% - 5px);
+          `;
+        case 'left':
+          return css`
+            border-color: transparent transparent transparent rgba(var(--sidooh-dark-rgb), .7);
+            left: 100%;
+            top: calc(50% - 5px);
+          `;
+        case 'right':
+          return css`
+            border-color: transparent rgba(var(--sidooh-dark-rgb), .7) transparent transparent;
+            right: 100%;
+            top: calc(50% - 5px);
+          `;
+        default:
+          return css`
+            top: 100%;
+            left: calc(50% - 4.5px);
+          `;
+      }
+    }}
   }
 `;
 
-const TooltipText = styled.div`
-  position: absolute;
-  width: 15rem;
-  font-size: 11px;
-  padding: 4px 8px;
-  border-radius: 10px;
-  background: rgba(15, 27, 76, .7);
-  color: #fff;
-  text-align: center;
-  display: none;
-  word-wrap: break-word;
-  z-index: 999;
-`;
-
 const Tooltip = ({ title, children, placement = 'bottom' }: TooltipProps) => {
+    const [isHovered, setIsHovered] = useState(false);
+    const [isFocused, setIsFocused] = useState(false);
+
+    const show = isHovered || isFocused;
+
+    const targetRef = useRef(null);
+
     return (
-        <TooltipTooltip placement={placement}>
-            {children}
-            <TooltipText>{title}</TooltipText>
-        </TooltipTooltip>
+        <TooltipWrapper>
+            <TooltipTarget ref={targetRef} onMouseEnter={() => setIsHovered(true)}
+                           onMouseLeave={() => setIsHovered(false)}
+                           onFocus={() => setIsFocused(true)}
+                           onBlur={() => setIsFocused(false)} showOnFocus={isFocused}>{children}</TooltipTarget>
+            {show && (
+                <CenterContainer placement={placement}>
+                    <TooltipBox placement={placement}>{title}</TooltipBox>
+                </CenterContainer>
+            )}
+        </TooltipWrapper>
     );
 };
 
--- src/stories/Tooltip.stories.tsx
+++ src/stories/Tooltip.stories.tsx
@@ -26,10 +26,27 @@ stories.add('Tooltip', () => {
 
             <Card.Body className={'d-flex justify-content-center p-5'}>
                 <Tooltip
-                    title={`Congratulations! You have received Ksh100.00 airtime from Sidooh account 254723604233 on 06/09/22, 7:05 AM. Sidooh Makes You Money with Every Purchase. Dial *384*99# NOW for FREE on your Safaricom line to BUY AIRTIME & START EARNING from your purchases.`}
+                    title={<span style={{
+                        display: "-webkit-box",
+                        overflow: "hidden",
+                        WebkitBoxOrient: "vertical",
+                        WebkitLineClamp: 2,
+                        cursor: "context-menu",
+                        maxWidth: '25rem'
+                    }}>
+                        Congratulations! You have received Ksh100.00 airtime from Sidooh account 254723604233 on 06/09/22, 7:05 AM. Sidooh Makes You Money with Every Purchase. Dial *384*99# NOW for FREE on your Safaricom line to BUY AIRTIME & START EARNING from your purchases.`
+                    </span>}
                     placement={'left'}>
                     <Button className={'me-1'}>Tooltip Left</Button>
                 </Tooltip>
+            </Card.Body>
+
+            <Card.Body className={'d-flex justify-content-center p-5'}>
+                <p>
+                    I am a {' '}
+                    <Tooltip title={`I am a Tooltip`}><b>Tooltip</b></Tooltip> {' '}
+                    within a paragraph.
+                </p>
             </Card.Body>
         </Card>
     );
