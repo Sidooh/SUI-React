bbabf0aca240569526badccca7215cce4b75a99c
Nabcellent <nabcellent.dev@gmail.com>
2022-11-16 07:57:23 +0300

    Updates Logo to include service name
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,28 +3,35 @@ import classNames from 'classnames';
 
 type LogoType = {
     src: string,
+    serviceName?: string,
     at?: 'navbar-vertical' | 'navbar-top' | 'auth',
     width?: number
     className?: string,
     textClass?: string
 }
 
-const Logo = ({src, at = 'auth', width = 78, className, textClass, ...rest}: LogoType) => {
+const Logo = ({ src, at = 'auth', width = 78, className, textClass, serviceName, ...rest }: LogoType) => {
     return (
         <a href="/" className={classNames(
             'text-decoration-none',
-            {'navbar-brand text-left': at === 'navbar-vertical'},
-            {'navbar-brand text-left': at === 'navbar-top'}
+            { 'navbar-brand text-left': at === 'navbar-vertical' },
+            { 'navbar-brand text-left': at === 'navbar-top' }
         )} {...rest}>
             <div className={classNames(
                 'd-flex', {
                     'align-items-center py-3': at === 'navbar-vertical',
                     'align-items-center': at === 'navbar-top',
-                    'flex-center fw-bolder fs-5 mb-4': at === 'auth'
+                    'flex-center fw-bolder fs-5 mb-4': at === 'auth',
+                    'flex-column': serviceName
                 },
                 className
             )}>
                 <img className="me-2" src={src} alt="Logo" width={width}/>
+                {serviceName && <h6 style={{
+                    fontFamily: "'Avenir Regular', sans-serif",
+                    margin: `-.${width * .4}rem 0 0`,
+                    fontSize: `${width * .1}pt`
+                }}>{serviceName}</h6>}
             </div>
         </a>
     );
--- src/stories/Logo.stories.tsx
+++ src/stories/Logo.stories.tsx
@@ -5,11 +5,24 @@ import Logo from '../components/Logo';
 
 const stories = storiesOf('Logo', module);
 
+const logos: { width: number, serviceName?: string }[] = [
+    { width: 50, serviceName: 'Products' },
+    { width: 78, serviceName: 'Payments' },
+    { width: 110, serviceName: 'Savings' },
+    { width: 150 },
+    { width: 200, serviceName: 'Notify' },
+]
+
+// 50, 78, 110, 150, 200
+
 stories.add('Logo', () => {
     return (
         <Card>
             <Card.Body>
-                {[50, 78, 110, 150, 200].map(w => <Logo src={require('../assets/images/logos/sidooh.png')} width={w}/>)}
+                {logos.map(l => (
+                    <Logo src={require('../assets/images/logos/sidooh.png')} serviceName={l.serviceName}
+                          width={l.width}/>
+                ))}
             </Card.Body>
         </Card>
     );
