c749595107545f5915d40776e0b5ef8ffec34eba
Nabcellent <nabcellent.dev@gmail.com>
2023-03-24 11:22:37 +0300

    Updates chart aid
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/utils/ChartAid.ts
+++ src/utils/ChartAid.ts
@@ -13,6 +13,7 @@ export class ChartAid {
     }
 
     getDataset = (raw: RawAnalytics[], freqCount?: number) => {
+        console.log(raw, this.#aggregateProperty)
         if (this.#timeIsUTC) {
             raw = raw.map(r => ({
                 date: Number(moment(r.date, 'YYYYMMDDHH').add(3, 'h').format('YYYYMMDDHH')),
