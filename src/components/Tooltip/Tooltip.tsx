108ad4278549f843010860042158abdc8a537331
Nabcellent <nabcellent.dev@gmail.com>
2023-04-23 10:16:00 +0300

    Upgrades tooltip component
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,141 +1,69 @@
-import { ReactNode, useRef, useState } from 'react';
-import styled, { css } from 'styled-components';
-
-type Position = 'top' | 'left' | 'bottom' | 'right'
+import styled, { css } from "styled-components";
+import { FC, ReactNode } from "react";
 
 type TooltipProps = {
-    title: ReactNode
-    children: ReactNode
-    placement?: Position
-}
+    title: ReactNode;
+    children: ReactNode;
+    placement?: "top" | "bottom" | "start" | "end";
+};
 
-const TooltipWrapper = styled.div`
+type TooltipWrapperProps = {
+    placement?: "top" | "bottom" | "start" | "end";
+    backgroundColor: string;
+};
+
+const TooltipWrapper = styled.div<TooltipWrapperProps>`
+  display: inline-block;
   position: relative;
-  display: inline-flex;
-  z-index: 1700;
-`;
+  z-index: 1;
 
-const TooltipTarget = styled.span<{ showOnFocus: boolean }>`
-  border: none;
-  background: none;
-  padding: 5px;
-  margin: -1px;
-  color: inherit;
-  cursor: inherit;
-  display: flex;
-  font-size: inherit;
-  ${({ showOnFocus }) => !showOnFocus && css`outline: none`}
-`;
+  &:hover span {
+    visibility: visible;
+    opacity: 1;
+  }
 
-const CenterContainer = styled.div<{ placement: Position }>`
-  position: absolute;
-  width: 200px;
-  margin-left: -100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  pointer-events: none;
-
-  ${({ placement }) => {
-    switch (placement) {
-      case 'bottom':
-        return css`
-          top: calc(100% + 5px);
-          left: 50%;
-        `;
-      case 'left':
-        return css`
-          margin-left: 0;
-          top: 50%;
-          transform: translateY(-50%);
-          right: calc(100% + 5px);
-          width: max-content;
-        `;
-      case 'right':
-        return css`
-          margin-left: 0;
-          top: 50%;
-          transform: translateY(-50%);
-          left: calc(100% + 5px);
-          width: max-content;
-        `;
-      default:
-        return css`
-          bottom: calc(100% + 5px);
-          left: 50%;
-        `;
-    }
-  }}
-`;
-
-const TooltipBox = styled.span<{ placement: Position }>`
-  position: relative;
-  background-color: rgba(var(--sidooh-dark-rgb), .8);
-  color: #fff;
-  text-align: center;
-  border-radius: .3rem;
-  padding: 5px 7px;
-  font-size: 14px;
-  box-shadow: var(--sidooh-box-shadow);
-
-  &:after {
-    content: '';
+  span {
+    visibility: hidden;
+    font-size: 9pt;
+    color: #fff;
+    background-color: rgba(97, 97, 97, 0.9);
+    border-radius: 4px;
+    padding: 4px 8px;
     position: absolute;
-    width: 1px;
-    height: 1px;
-    border-width: 5px;
-    border-style: solid;
-    border-color: rgba(var(--sidooh-dark-rgb), .8) transparent transparent transparent;
+    z-index: 1;
+    opacity: 0;
+    transition: opacity 0.2s ease-out, visibility 0.2s ease-out;
+    max-width: 300px;
+    pointer-events: none;
+    width: max-content;
 
-    ${({ placement }) => {
-      switch (placement) {
-        case 'bottom':
-          return css`
-            border-color: transparent transparent rgba(var(--sidooh-dark-rgb), .8) transparent;
-            bottom: 100%;
-            left: calc(50% - 5px);
-          `;
-        case 'left':
-          return css`
-            border-color: transparent transparent transparent rgba(var(--sidooh-dark-rgb), .8);
-            left: 100%;
-            top: calc(50% - 5px);
-          `;
-        case 'right':
-          return css`
-            border-color: transparent rgba(var(--sidooh-dark-rgb), .8) transparent transparent;
-            right: 100%;
-            top: calc(50% - 5px);
-          `;
-        default:
-          return css`
-            top: 100%;
-            left: calc(50% - 4.5px);
-          `;
-      }
-    }}
+    ${(props) => props.placement === "top" && css`
+      bottom: 125%;
+      left: 50%;
+      transform: translateX(-50%);
+    `}
+    ${(props) => props.placement === "bottom" && css`
+      top: 125%;
+      left: 50%;
+      transform: translateX(-50%);
+    `}
+    ${(props) => props.placement === "start" && css`
+      top: 50%;
+      right: 125%;
+      transform: translateY(-50%);
+    `}
+    ${(props) => props.placement === "end" && css`
+      top: 50%;
+      left: 125%;
+      transform: translateY(-50%);
+    `}
   }
 `;
 
-const Tooltip = ({ title, children, placement = 'bottom' }: TooltipProps) => {
-    const [isHovered, setIsHovered] = useState(false);
-    const [isFocused, setIsFocused] = useState(false);
-
-    const show = isHovered || isFocused;
-
-    const targetRef = useRef(null);
-
+const Tooltip: FC<TooltipProps> = ({ title, placement = "top", children }) => {
     return (
-        <TooltipWrapper>
-            <TooltipTarget ref={targetRef} onMouseEnter={() => setIsHovered(true)}
-                           onMouseLeave={() => setIsHovered(false)}
-                           onFocus={() => setIsFocused(true)}
-                           onBlur={() => setIsFocused(false)} showOnFocus={isFocused}>{children}</TooltipTarget>
-            {show && (
-                <CenterContainer placement={placement}>
-                    <TooltipBox placement={placement}>{title}</TooltipBox>
-                </CenterContainer>
-            )}
+        <TooltipWrapper placement={placement} backgroundColor="rgba(97, 97, 97, 0.9)">
+            {children} {title && <span dangerouslySetInnerHTML={{ __html: title }}/>}
         </TooltipWrapper>
     );
 };
