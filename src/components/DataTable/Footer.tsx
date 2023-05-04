f117405ef659c07b3d75cd7484985f186b7ec473
Nabcellent <nabcellent.dev@gmail.com>
2023-05-04 16:17:19 +0300

    Improves pagination info display
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/components/DataTable/Footer.tsx
+++ src/components/DataTable/Footer.tsx
@@ -54,10 +54,12 @@ const Footer = ({
         table.setPageIndex(page)
     }
 
+    const hasServerPagination = serverTotal && serverPageCount && currentServerPage && onPreviousServerPage
+
     return (
         <SimpleBarReact className={'text-nowrap px-3'}>
             <Row className={'align-items-center justify-content-center g-0'}>
-                {serverTotal && serverPageCount && currentServerPage && onPreviousServerPage ? (
+                {hasServerPagination ? (
                     <Col className={"d-flex align-items-center fs--1"}>
                         {serverPageCount && currentServerPage && onPreviousServerPage ? (
                             <Tooltip title={'Previous Page'}>
@@ -77,13 +79,17 @@ const Footer = ({
                         ) : ''}
                         <span className="mx-2 border-end">&nbsp;</span>
                         <b>{currentServerPage} of {serverPageCount}</b>
-                        <span className="mx-1 border-end">&nbsp;</span>
-                        <FormSelect size="sm" className="w-auto border-0" value={serverPageSize}
-                                    onChange={(e: ChangeEvent<HTMLSelectElement>) => onSetServerPageSize && onSetServerPageSize(Number(e.target.value))}>
-                            {[50, 100, 200, 500, 1000].map(pageSize => (
-                                <option key={pageSize} value={pageSize}>{pageSize}</option>
-                            ))}
-                        </FormSelect>
+                        {onSetServerPageSize && (
+                            <>
+                                <span className="mx-1 border-end">&nbsp;</span>
+                                <FormSelect size="sm" className="w-auto border-0" value={serverPageSize}
+                                            onChange={(e: ChangeEvent<HTMLSelectElement>) => onSetServerPageSize(Number(e.target.value))}>
+                                    {[50, 100, 200, 500, 1000].map(pageSize => (
+                                        <option key={pageSize} value={pageSize}>{pageSize}</option>
+                                    ))}
+                                </FormSelect>
+                            </>
+                        )}
                         {onGoToServerPage && serverPageCount > 3 && (
                             <>
                                 <span className="mx-1 border-end">&nbsp;</span>
@@ -108,51 +114,56 @@ const Footer = ({
                         )}
                     </Col>
                 ) : ''}
-                <Col className={'d-flex align-items-center justify-content-md-end fs--1'}>
-                    <span>Total: <b>{table.getCoreRowModel().rows.length.toLocaleString()}</b></span>
-                    <span className="mx-1 border-end">&nbsp;</span>
-                    {table.getPageCount() > 3 && (
-                        <>
-                            <span className="flex items-center gap-1">Go to pg</span>
-                            <input type="number" value={table.getState().pagination.pageIndex + 1}
-                                   onChange={(e) => goToPage(e)}
-                                   step="1" min="1" max={table.getPageCount()}
-                                   className="form-control form-control-sm w-auto ms-1"/>
-                            <span className="mx-1 border-end"> &nbsp;</span>
-                        </>
-                    )}
-                    <b>{table.getState().pagination.pageIndex + 1} of {table.getPageCount()}</b>
-                    <span className="mx-1 border-end">&nbsp;</span>
-                    <IconButton size={'sm'} disabled={!table.getCanPreviousPage()}
-                                onClick={() => table.setPageIndex(0)}>
-                        <FaAngleDoubleLeft/>
-                    </IconButton>
-                    <IconButton size={'sm'} className={'ms-1'} disabled={!table.getCanPreviousPage()}
-                                onClick={() => table.previousPage()}>
-                        <FaAngleLeft/>
-                    </IconButton>
-                    <FormSelect size="sm" className="w-auto mx-2 border-0" value={table.getState().pagination.pageSize}
-                                onChange={(e: ChangeEvent<HTMLSelectElement>) => table.setPageSize(Number(e.target.value))}>
-                        {[5, 10, 20, 50, 100].map(pageSize => (
-                            <option key={pageSize} value={pageSize}>{pageSize}</option>
-                        ))}
-                    </FormSelect>
-                    <IconButton size={'sm'} className={'ms-1'} disabled={!table.getCanNextPage()}
-                                onClick={() => table.nextPage()}>
-                        <FaAngleRight/>
-                    </IconButton>
-                    <IconButton size={'sm'} className={'ms-1'} disabled={!table.getCanNextPage()}
-                                onClick={() => table.setPageIndex(table.getPageCount() - 1)}>
-                        <FaAngleDoubleRight/>
-                    </IconButton>
-                    {onViewAll && (
-                        <>
-                            <p className="mb-0 mx-1 border-end"> &nbsp;</p>
-                            <Tooltip title={'View All'}>
-                                <IconButton onClick={onViewAll}><MdReadMore/></IconButton>
-                            </Tooltip>
-                        </>
-                    )}
+                <Col
+                    className={`d-flex align-items-center ${hasServerPagination ? 'justify-content-md-end' : 'justify-content-between'} fs--1`}>
+                    <div className={'d-flex align-items-center'}>
+                        <span>Total: <b>{table.getCoreRowModel().rows.length.toLocaleString()}</b></span>
+                        <span className="mx-1 border-end">&nbsp;</span>
+                        {table.getPageCount() > 3 && (
+                            <>
+                                <span className="flex items-center gap-1">Go to pg</span>
+                                <input type="number" value={table.getState().pagination.pageIndex + 1}
+                                       onChange={(e) => goToPage(e)}
+                                       step="1" min="1" max={table.getPageCount()}
+                                       className="form-control form-control-sm w-auto ms-1"/>
+                                <span className="mx-1 border-end"> &nbsp;</span>
+                            </>
+                        )}
+                        <b>{table.getState().pagination.pageIndex + 1} of {table.getPageCount()}</b>
+                    </div>
+                    <div className={'d-flex align-items-center'}>
+                        <span className="mx-1 border-end">&nbsp;</span>
+                        <IconButton size={'sm'} disabled={!table.getCanPreviousPage()}
+                                    onClick={() => table.setPageIndex(0)}>
+                            <FaAngleDoubleLeft/>
+                        </IconButton>
+                        <IconButton size={'sm'} className={'ms-1'} disabled={!table.getCanPreviousPage()}
+                                    onClick={() => table.previousPage()}>
+                            <FaAngleLeft/>
+                        </IconButton>
+                        <FormSelect size="sm" className="w-auto mx-2 border-0" value={table.getState().pagination.pageSize}
+                                    onChange={(e: ChangeEvent<HTMLSelectElement>) => table.setPageSize(Number(e.target.value))}>
+                            {[5, 10, 20, 50, 100].map(pageSize => (
+                                <option key={pageSize} value={pageSize}>{pageSize}</option>
+                            ))}
+                        </FormSelect>
+                        <IconButton size={'sm'} className={'ms-1'} disabled={!table.getCanNextPage()}
+                                    onClick={() => table.nextPage()}>
+                            <FaAngleRight/>
+                        </IconButton>
+                        <IconButton size={'sm'} className={'ms-1'} disabled={!table.getCanNextPage()}
+                                    onClick={() => table.setPageIndex(table.getPageCount() - 1)}>
+                            <FaAngleDoubleRight/>
+                        </IconButton>
+                        {onViewAll && (
+                            <>
+                                <p className="mb-0 mx-1 border-end"> &nbsp;</p>
+                                <Tooltip title={'View All'}>
+                                    <IconButton onClick={onViewAll}><MdReadMore/></IconButton>
+                                </Tooltip>
+                            </>
+                        )}
+                    </div>
                 </Col>
             </Row>
         </SimpleBarReact>
