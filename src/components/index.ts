800f3b9314a390c314b8b6fa05953bd97993138b
nabcellent <nabcellent.dev@gmail.com>
2024-06-05 12:12:28 +0300

    New: ThemeProvider Component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
-export * from './Phone';
 export { default as DataTable } from './DataTable';
 export { default as SubmitButton } from './SubmitButton';
 export { default as Logo } from './Logo';
@@ -6,5 +5,7 @@ export { default as TableDate } from './TableDate';
 export { default as Tooltip } from './Tooltip';
 export { default as Waffle } from './Waffle';
 export { default as PageLoader } from './PageLoader';
+export * from './Phone';
+export * from './ThemeProvider';
 
 export { ErrorBoundary, ErrorFallback } from './Error';
